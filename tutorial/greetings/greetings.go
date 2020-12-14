7b3ab34f411d4d970dcdcd3403b565286ddb4f74
yvvyoon <yoon4480@naver.com>
2020-12-14 15:41:15 +0900

    Add: 에러 핸들링

--- tutorial/greetings/greetings.go
+++ tutorial/greetings/greetings.go
@@ -1,12 +1,19 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	message := fmt.Sprintf("Hi, %v.  Welcome!", name)
 	// := 연산자는 선언과 할당을 동시에 함
 	// var message string
 	// message = fmt.Sprintf("Hi, %v.  Welcome!", name)
 
-	return message
+	return message, nil
 }
--- tutorial/hello/hello.go
+++ tutorial/hello/hello.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Yoon")
+	log.SetPrefix("greetings: ")
+	// 타임스탬프 또는 소스 파일 정보 미포함
+	log.SetFlags(0)
+	// flag 1: greetings: 2020/12/14 empty name
+	// flag 2: greetings: 15:38:28 empty name
+	// flag 3: greetings: 2020/12/14 15:38:28 empty name
+	// flag 4: greetings: 2020/12/14 15:38:28.564935 empty name
+
+	message, err := greetings.Hello("")
+
+	if err != nil {
+		// error 로그 출력과 함께 프로그램 종료
+		log.Fatal(err)
+	}
 
 	fmt.Println(message)
 }
